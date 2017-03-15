d648d46c6e9bc0e806254a57b02ee7ce911d934e
Mathieu Bourque <skymatb@gmail.com>
2017-03-15 17:32:53 +0100

    port distant

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
   });
 });
 
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Server actif sur port 8080');
 });
 
